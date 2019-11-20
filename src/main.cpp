16fa97061324effbb05999939a26e8e57c88630e
椎名深雪 <xiaochun.tong@mail.utoronto.ca>
2019-11-19 22:31:02 -0500

    added imgui

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,17 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <PerlinNoise.hpp>
+#include <imgui.h>
+#include <examples/imgui_impl_glfw.h>
+#include <examples/imgui_impl_opengl3.h>
+#include <memory>
 
 using namespace glm;
 double glfwMouseScrollY;
 
 struct Material {
+    vec3 emission;
     vec3 baseColor;
     float roughness;
     float metalness;
@@ -49,12 +55,20 @@ const char *fragmentShaderSource = R"(
 out vec4 FragColor;
 layout(binding = 0) uniform sampler3D world;
 layout(binding = 1, rgba32f)  uniform image2D accumlatedImage;
-
+layout(binding = 2, rgba32f)  uniform image2D seeds;
 uniform vec2 iResolution;
 uniform mat4 cameraOrigin;
 uniform mat4 cameraDirection;
 uniform ivec3 worldDimension;
 uniform int iTime;
+
+struct Material {
+    vec3 emission;
+    vec3 baseColor;
+    float roughness;
+    float metalness;
+    float specular;
+};
 
 
 const float M_PI = 3.1415926535;
@@ -75,7 +89,7 @@ float intersectBox(vec3 o, vec3 d, vec3 p1, vec3 p2, out vec3 n){
     vec3 tmax = max(t0, t1);
     float t = maxComp(tmin);
     if(t < minComp(tmax)){
-    	t = max(t, RayBias);
+    	t = max(t, 0.0);
         if(t > minComp(tmax)){
         	return -1.0;
         }
@@ -101,6 +115,7 @@ struct Intersection{
     float t;
     vec3 n;
     vec3 p;
+    Material mat;
 };
 bool insideWorld(vec3 p){
     return all(lessThan(p, vec3(worldDimension)+vec3(1))) && all(greaterThanEqual(p, vec3(-2)));
@@ -113,7 +128,7 @@ int map(vec3 p){
 bool intersect1(vec3 ro, vec3 rd, out Intersection isct)
 {
     vec3 n;
-    float distance = intersectBox(ro, rd, vec3(-1), vec3(worldDimension), n);
+    float distance = intersectBox(ro, rd, vec3(-1.5), vec3(worldDimension)+vec3(0.5), n);
     if(distance < 0.0){
         return false;
     }
@@ -129,7 +144,8 @@ bool intersect1(vec3 ro, vec3 rd, out Intersection isct)
     isct.n = n;
 	vec3 mask = vec3(0, 0, 0);
 	float t = 0;
-	for (int i = 0; i < 128; ++i) {
+    int maxIter = int(dot(worldDimension, ivec3(1)));
+	for (int i = 0; i < maxIter; ++i) {
 		if (!insideWorld(p)) {
 			break;
 		}
@@ -140,7 +156,12 @@ bool intersect1(vec3 ro, vec3 rd, out Intersection isct)
 			// find normal
 			isct.t = distance + t;
 			isct.n = -sign(rd) * mask;
-
+            isct.mat.baseColor = vec3(83, 135, 219)/255.0;
+            if(mat == 2){
+                isct.mat.emission = vec3(3);
+            }else{
+                isct.mat.emission = vec3(0);
+            }
 			return true;
 		}
 		if (tMax.x < tMax.y) {
@@ -185,6 +206,7 @@ bool intersect(vec3 ro, vec3 rd, out Intersection isct){
     isct.t = t;
     isct.p = ro + t * rd;
     isct.n = vec3(0,1,0);
+    isct.mat.baseColor = vec3(1);
     return true;
 }
 
@@ -205,19 +227,17 @@ vec2 random2(vec3 st){
 
 struct Sampler{
     int dimension;
-    vec2 seed;
+    uint seed;
 };
 
 float nextFloat(inout Sampler sampler){
-    float v = random2(vec3(sampler.seed, sampler.dimension)).x;
+    sampler.seed = (1103515245*(sampler.seed) + 12345);
     sampler.dimension++;
-    return v;
+    return float(sampler.seed) * 2.3283064370807974e-10;
 }
 
 vec2 nextFloat2(inout Sampler sampler){
-    vec2 v = random2(vec3(sampler.seed, sampler.dimension));
-    sampler.dimension++;
-    return v;
+    return vec2(nextFloat(sampler), nextFloat(sampler));
 }
 struct LocalFrame{
     vec3 N, T, B;
@@ -257,6 +277,8 @@ vec3 cosineHemisphereSampling(vec2 u){
 vec3 LiBackground(vec3 o, vec3 d){
     return vec3(0);
 }
+#define AO
+#ifdef AO
 vec3 Li(vec3 o, vec3 d, inout Sampler sampler) {
     Intersection isct;
     float tmax = 100.0f;
@@ -274,24 +296,55 @@ vec3 Li(vec3 o, vec3 d, inout Sampler sampler) {
     }
     return vec3(0);
 }
-
+#else
+const int maxDepth = 5;
+vec3 Li(vec3 o, vec3 d, inout Sampler sampler) {
+    Intersection isct;
+    float tmax = 100.0f;
+    vec3 L = vec3(0);
+    vec3 beta = vec3(1);
+    for(int depth = 0;depth < maxDepth;depth++){
+        if(!intersect(o, d, isct)){
+            L += beta * LiBackground(o, d);
+            break;
+        }
+        L += beta * isct.mat.emission;
+        LocalFrame frame;
+        computeLocalFrame(isct.n, frame);
+        vec3 wi = cosineHemisphereSampling(nextFloat2(sampler));
+        wi = localToWorld(wi, frame);
+        o = isct.p + RayBias * wi;
+        d = wi;
+        float pdf = abs(dot(isct.n, wi)) / M_PI;
+        beta *= isct.mat.baseColor * abs(dot(isct.n, wi)) / pdf;
+    }
+    return L;
+}
+#endif
+vec3 removeNaN(vec3 v){
+    return mix(v, vec3(0), isnan(v));
+}
 void main() {
     vec2 uv = gl_FragCoord.xy / iResolution;
     Sampler sampler;
     sampler.dimension = 0;
-    sampler.seed = uv + vec2(0.0, iTime);
+
+    sampler.seed = floatBitsToUint(imageLoad(seeds, ivec2(gl_FragCoord.xy)).r);
 
     uv = 2.0 * uv - vec2(1.0);
     uv.x *= iResolution.x / iResolution.y;
     vec4 _o = (cameraOrigin * vec4(vec3(0), 1));
     vec3 o = _o.xyz / _o.w;
-    vec3 d = normalize(mat3(cameraDirection) * normalize(vec3(uv, 1) - vec3(0)));
-    vec4 color = vec4(Li(o, d, sampler), 1.0);
+    float fov = 60.0 / 180.0 * M_PI;
+    float z = 1.0 / tan(fov / 2.0);
+    vec3 d = normalize(mat3(cameraDirection) * normalize(vec3(uv, z) - vec3(0,0,0)));
+    vec4 color = vec4(removeNaN(Li(o, d, sampler)), 1.0);
     vec4 prevColor = imageLoad(accumlatedImage,  ivec2(gl_FragCoord.xy));
     if(iTime > 0)
         color += prevColor;
-    FragColor = vec4(color.rgb / color.a, 1.0);
+    FragColor = vec4(pow(color.rgb / color.a,vec3(1.0/2.2)), 1.0);
     imageStore(accumlatedImage,  ivec2(gl_FragCoord.xy), color);
+    imageStore(seeds, ivec2(gl_FragCoord.xy), vec4(uintBitsToFloat(sampler.seed)));
 }
 )";
 
@@ -301,13 +354,29 @@ struct World {
     GLuint world;
 
     void initData() {
+#pragma  omp parallel for default(none)
         for (int x = 0; x < worldDimension.x; x++) {
+            siv::PerlinNoise perlin;
             for (int y = 0; y < worldDimension.y; y++) {
                 for (int z = 0; z < worldDimension.z; z++) {
-                    vec3 p = vec3(x, y, z) / vec3(worldDimension);
-                    p = 2.0f * p - vec3(1);
-                    if (length(p) < 0.4) {
-                        (*this)(x, y, z) = 1;
+                    vec3 p = vec3(x, y, z);// / vec3(worldDimension);
+                    //p = 2.0f * p - vec3(1);
+//                    if (length(p) < 0.4) {
+//                        if(abs(p.x)<0.1)
+//                            (*this)(x, y, z) = 2;
+//                        else
+//                            (*this)(x, y, z) = 1;
+//                    } else {
+//                        (*this)(x, y, z) = 0;
+//                    }
+                    p *= 0.1f;
+                    auto n = perlin.noise0_1(p.x, p.y, p.z);
+                    if (0.6 < n && n < 0.8) {
+                        if (n > 0.73) {
+                            (*this)(x, y, z) = 2;
+                        } else {
+                            (*this)(x, y, z) = 1;
+                        }
                     } else {
                         (*this)(x, y, z) = 0;
                     }
@@ -352,6 +421,7 @@ struct World {
 struct Renderer {
     GLint program;
     GLuint VBO;
+    GLuint seed;
     World world;
     mat4 cameraDirection, cameraOrigin;
     GLuint sample; // texture for 1 spp
@@ -360,7 +430,7 @@ struct Renderer {
     ivec2 mousePos, prevMousePos, lastFrameMousePos;
     bool prevMouseDown = false;
     int iTime = 0;
-    vec2 eulerAngle = vec2(0,0);
+    vec2 eulerAngle = vec2(0, 0);
 
     explicit Renderer() : world(ivec3(50, 50, 50)) {
     }
@@ -430,6 +500,22 @@ struct Renderer {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, 1280, 720, 0, GL_RGBA,
                      GL_FLOAT, NULL);
 
+        std::vector<float> seeds;
+        for (size_t i = 0; i < 1920 * 1080; i++) {
+            seeds.emplace_back(uintBitsToFloat(rand()));
+            seeds.emplace_back(uintBitsToFloat(rand()));
+            seeds.emplace_back(uintBitsToFloat(rand()));
+            seeds.emplace_back(uintBitsToFloat(rand()));
+        }
+        glGenTextures(1, &seed);
+        glBindTexture(GL_TEXTURE_2D, seed);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, 1920, 1080, 0, GL_RGBA,
+                     GL_FLOAT, seeds.data());
 
     }
 
@@ -486,7 +572,7 @@ struct Renderer {
             auto tr = vec3(world.worldDimension) * 0.5f;
             tr.z *= -1.0f;
             cameraDirection = M;
-            cameraOrigin = translate(vec3(tr.x,tr.y,-tr.z)) * cameraDirection * translate(vec3(0, 0, tr.z));
+            cameraOrigin = translate(vec3(tr.x, tr.y, -tr.z)) * cameraDirection * translate(vec3(0, 0, 3.5 * tr.z));
 
             prevMouseDown = state == GLFW_PRESS;
             lastFrameMousePos = ivec2(xpos, ypos);
@@ -501,6 +587,8 @@ struct Renderer {
         glBindTexture(GL_TEXTURE_3D, world.world);
         glBindTexture(GL_TEXTURE_2D, accum);
         glBindImageTexture(1, accum, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+        glBindTexture(GL_TEXTURE_2D, seed);
+        glBindImageTexture(2, seed, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
         glUniform1i(glGetUniformLocation(program, "world"), 0);
         glUniform2f(glGetUniformLocation(program, "iResolution"), w, h);
         glUniform3i(glGetUniformLocation(program, "worldDimension"),
@@ -513,42 +601,109 @@ struct Renderer {
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+        if (iTime % 200 == 0)
+            printf("pass = %d\n", iTime);
+    }
+};
+
+struct Application {
+    GLFWwindow *window;
+    std::unique_ptr<Renderer> renderer;
+
+    Application() {
+        if (!glfwInit()) {
+            fprintf(stderr, "failed to init glfw");
+            exit(1);
+        }
+        window = glfwCreateWindow(1280, 720, "NanoVoxel", nullptr, nullptr);
+        glfwMakeContextCurrent(window);
+        glfwSetScrollCallback(window, [](GLFWwindow *window, double xoffset, double yoffset) {
+            glfwMouseScrollY = yoffset;
+        });
+        glfwSwapInterval(0);
+        if (0 != gl3wInit()) {
+            fprintf(stderr, "failed to init gl3w");
+            exit(1);
+        }
+
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_TEXTURE_3D);
+        glDebugMessageCallback(MessageCallback, 0);
+
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &io = ImGui::GetIO();
+        (void) io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+        //io.ConfigViewportsNoAutoMerge = true;
+        //io.ConfigViewportsNoTaskBarIcon = true;
+
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+        //ImGui::StyleColorsClassic();
+
+        // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+        ImGuiStyle &style = ImGui::GetStyle();
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            style.WindowRounding = 0.0f;
+            style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        }
+
+        // Setup Platform/Renderer bindings
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init("#version 430");
+
+        renderer = std::make_unique<Renderer>();
+        renderer->compileShader();
+        renderer->setUpWorld();
+        renderer->setUpVBO();
+
+    }
+
+    void show() {
+        ImGuiIO& io = ImGui::GetIO();
+        while (!glfwWindowShouldClose(window)) {
+
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(0,0,0,0);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+            if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+            {
+                GLFWwindow* backup_current_context = glfwGetCurrentContext();
+                ImGui::UpdatePlatformWindows();
+                ImGui::RenderPlatformWindowsDefault();
+                glfwMakeContextCurrent(backup_current_context);
+            }
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+            renderer->render(window);
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+        glfwTerminate();
     }
 };
 
 int main(int argc, char **argv) {
-    if (!glfwInit()) {
-        fprintf(stderr, "failed to init glfw");
-        exit(1);
-    }
-    GLFWwindow *window = glfwCreateWindow(1280, 720, "NanoVoxel", nullptr, nullptr);
-    glfwMakeContextCurrent(window);
-    glfwSetScrollCallback(window,[](GLFWwindow*window, double xoffset, double yoffset){
-        glfwMouseScrollY = yoffset;
-    });
+    Application app;
+    app.show();
 
-    if (0 != gl3wInit()) {
-        fprintf(stderr, "failed to init gl3w");
-        exit(1);
-    }
-    glEnable(GL_DEBUG_OUTPUT);
-    glEnable(GL_TEXTURE_3D);
-    glDebugMessageCallback(MessageCallback, 0);
-    Renderer renderer;
-    renderer.setUpVBO();
-    renderer.setUpWorld();
-    renderer.compileShader();
 
-    while (!glfwWindowShouldClose(window)) {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-        renderer.render(window);
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-    glfwTerminate();
     return 0;
 }
